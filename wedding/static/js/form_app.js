b3e8a54ee877734f4f8a6aa81d76864de5472eef
Alexis Durand <alexis.durand@iwoca.co.uk>
2016-12-22 16:54:15 +0100

    Added angular app for form

--- wedding/static/js/form_app.js
+++ wedding/static/js/form_app.js
@@ -2,6 +2,43 @@ angular.module('app', []).run();
 
 app = angular.module('app');
 
+app.controller('FormsController', function ($scope) {
+    $scope.world = 'world';
+    $scope.forms = [];
+    function addForm() {
+        $scope.forms.push({
+            'form': {},
+            'is_valid': false
+        })
+    }
+
+    addForm();
+
+    function removeForm(index) {
+        $scope.forms.splice(index, 1);
+    }
+});
+
 app.controller('FormController', function ($scope) {
-    $scope.world = 'world'
+    $scope.food_options = [
+        {'value': 'normal', 'label': 'Normal'},
+        {'value': 'vegetarian', 'label': 'Végétarien'},
+        {'value': 'halal', 'label': 'Halal'},
+        {'value': 'child', 'label': 'Enfant'},
+        {'value': 'baby', 'label': 'Bébé (pas de repas'},
+        {'value': 'allergies', 'label': 'Allergies'},
+    ]
+
+});
+
+app.directive('weddingForm', function () {
+    return {
+        restrict: 'E',
+        controller: 'FormController',
+        scope: {
+            'form': '=form'
+        },
+        templateUrl: '/static/html/form.html'
+    };
+
 });
